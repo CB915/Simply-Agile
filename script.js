49c15ebcc8819f2086adecec0ed49c4c389a8e4c
CB915 <101878146+CB915@users.noreply.github.com>
2023-09-02 15:30:31 -0400

    Fixed some responsivess issues

---
+++ script.js
@@ -0,0 +1,37 @@
+// Validate Function for validating signup form 
+
+console.log("script linked!");
+
+const form = document.querySelectorAll('#sign-up-form input');
+const submit = document.querySelector('#submit-btn');
+const usrPass = document.getElementById("password");
+const userPassConfrm = document.getElementById("confirm_password");
+        
+//Client side validation to check if passwords match (will need server side validation as well)
+function Validate(){
+    isValid = true;
+    console.log("Submit Button clicked");
+    //loop through all items in the form, only getting the inputs
+    for(x = 0; x < form.length; x++){
+        form[x].setAttribute("required","");
+    }
+    if(userPassConfrm.value != usrPass.value){
+      
+        userPassConfrm.setCustomValidity("Passwords must match");
+        console.log(usrPass.checkValidity() + "  " + usrPass.value);
+        console.log(userPassConfrm.checkValidity() + "  "+userPassConfrm.value);
+        usrPass.classList.add("invalid")
+        return false;
+
+    }else{
+        userPassConfrm.setCustomValidity("");
+        usrPass.classList.remove("invalid");
+    }
+    // Loops through and logs the style to the console 
+    if(isValid == true){
+        for(x = 0; x < form.length; x++){
+        console.log(form[x].id + ":" +form[x].value);
+    }
+        return isValid;
+    }
+}
